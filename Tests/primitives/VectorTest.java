dd00e198e4cbae848af9c635c356e3ca6b27efff
eliezergensburger <eliezer.jct@gmail.com>
2020-03-24 13:59:43 +0200

    recitation 2 24/03/2020

---
+++ Tests/primitives/VectorTest.java
@@ -0,0 +1,81 @@
+package primitives;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VectorTest {
+
+    @Test
+    void testadd() {
+        Vector v1 = new Vector(1.0, 1.0, 1.0);
+        Vector v2 = new Vector(-1.0, -1.0, -1.5);
+
+        v1 = v1.add(v2);
+        assertEquals(new Vector(0.0,0.0,-0.5),v1);
+
+        v2 = v2.add(v1);
+        assertEquals(new Vector(-1.0, -1.0, -2.0),v2);
+
+    }
+
+    @Test
+    void testsubtract() {
+    }
+
+    @Test
+    void testscale() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void dotProduct() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testcrossProduct() {
+
+        Vector v1 = new Vector(3.5, -5.0, 10.0);
+        Vector v2 = new Vector(2.5,7,0.5);
+        Vector v3 = v1.crossProduct(v2);
+
+        assertEquals( 0, v3.dotProduct(v2), 1e-10);
+        assertEquals( 0, v3.dotProduct(v1), 1e-10);
+
+        Vector v4 = v2.crossProduct(v1);
+
+        System.out.println(v3.toString());
+        System.out.println(v4.toString());
+
+//        v3 = v3.add(new Vector(0.0,0.0,0.5));
+        try {
+            v3.add(v4);
+            fail("Vector (0,0,0) not valid");
+        }
+        catch  (IllegalArgumentException e)
+        {
+            assertTrue(e.getMessage()!= null);
+        }
+        assertTrue(v3.length() >84);
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void length() {
+    }
+
+    @Test
+    void testnormalize() {
+        Vector v = new Vector(3.5, -5, 10);
+        v.normalize();
+        assertEquals(1, v.length(), 1e-10);
+
+        try {
+            Vector v1 = new Vector(0, 0, 0);
+            v.normalize();
+            fail("Didn't throw divide by zero exception!");
+        } catch (IllegalArgumentException ex) {
+            assertEquals("Point3D(0.0,0.0,0.0) not valid for vector head", ex.getMessage());
+        }
+        assertTrue(true);
+    }
+}
